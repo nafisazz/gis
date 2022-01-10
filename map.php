94b90112e6e1066dc07451aeb2f3758496712ee5
nafisaaz <nafisaazzahra25@gmail.com>
2022-01-10 22:00:08 +0700

    uas

---
+++ map.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bandar Lampung</title>
+</head>
+<body>
+    
+<div class="map">
+    <object data="http://localhost:8090/geoserver/Lampung/wms?service=WMS&version=1.1.0&request=GetMap&layers=Lampung:Group%20Peta%20Lampung&styles=&bbox=105.14678955078125,-5.54346227645874,105.3501205444336,-5.324850082397461&width=714&height=768&srs=EPSG:4326&format=application/openlayers" width="1000" height="900"
+     type="text/html"></object>
+</div>
